669bf2ae24fd1e569b7a7cad600f0665b980cf27
Juan D Lopez <juandiegolopezortiz4@gmail.com>
2025-07-21 21:25:19 -0500

    Se hace la conexión a la base de datos

---
+++ src/config/mongo.config.mjs
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const dbConnection = async () => {
+
+    try {
+         await mongoose.connect ('mongodb://localhost:27017/db-vet-place', {})
+
+         console.log ( 'Base de datos conectada correctamente' )
+    } catch (error) {
+        console.error ( error )
+        console.log ( 'Error al conectar a la base de datos' )
+    }
+   
+}
+
+export default dbConnection;
--- src/index.mjs
+++ src/index.mjs
@@ -1,5 +1,12 @@
 import express from "express";
+import dbConnection from "./config/mongo.config.mjs";
+
+
 const app = express()
+
+dbConnection()
+
+
 
 app.listen ( 3000, () => {
     console.log ( 'Servidor corriendo en el puerto 3000' )
